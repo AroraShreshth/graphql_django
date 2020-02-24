44f11500e4851db9ba110ed40053b3fd2bd69ebb
Shreshth Arora <sarora_be18@thapar.edu>
2020-02-25 02:50:22 +0530

    secrets

--- graphsite/settings.py
+++ graphsite/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ba3$-$0n3hkvwy*4gu$6esx668j28b&t-&)$27=2w(@73eoq9b'
-
+#SECRET_KEY = 'ba3$-$0n3hkvwy*4gu$6esx668j28b&t-&)$27=2w(@73eoq9b'
+SECRET_KEY = os.environ.get(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['shreshth-django-graphql.herokuapp.com']
 
 
 # Application definition
